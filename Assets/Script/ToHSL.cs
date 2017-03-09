ec826bee23cdc06e2f98daa22881347b5e0e0fc2
龚冰蕾 <403235717@qq.com>
2017-03-09 11:45:03 +0800

    加HSL变换

---
+++ Assets/Script/ToHSL.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ToHSL : MonoBehaviour{
+	private  Material _m;
+	public int minVal;
+	public int maxVal;
+	public float duration;
+	public int loop = -1;
+	private float _val;
+	public float val
+	{
+		get{return _val;}
+		set{_val = value;
+			_m.SetInt( "_Hue", (int)_val );}
+	}
+	// Use this for initialization
+	void Start () {
+		_m = GetComponent<SpriteRenderer>().material;
+		_val = minVal;
+		if(loop <= 0)
+			loop = int.MaxValue;
+		DOTween.To(()=>val, x =>{_val = x; _m.SetInt( "_Hue", (int)_val );}, maxVal, duration).SetLoops(loop, LoopType.Yoyo);
+	}
+}
